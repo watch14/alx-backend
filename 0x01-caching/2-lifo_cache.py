774b268f49285348c4d6fa18ef3480a18302f454
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-21 17:19:45 +0100

    fast_push

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" LIFO """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache """
+
+    def __init__(self):
+        """ LIFO cache """
+        super().__init__()
+        self.last_key = None
+
+    def put(self, key, item):
+        """ add an item in cache """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            if self.last_key:
+                print("DISCARD:", self.last_key)
+                del self.cache_data[self.last_key]
+
+        self.cache_data[key] = item
+        self.last_key = key
+
+    def get(self, key):
+        """ item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
+
+
+if __name__ == "__main__":
+    LIFOCache = LIFOCache
