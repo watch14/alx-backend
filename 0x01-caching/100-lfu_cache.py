de43dfe5913a895c9738fe6e50b9ad446c6eeec7
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-23 17:26:44 +0100

    fast_push

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -5,50 +5,51 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """ LFUCache """
+    """ LFUCache defines a caching system with LFU eviction policy """
 
     def __init__(self):
-        """ Initialize LFU cache """
+        """ Initialize the cache """
         super().__init__()
-        self.frequency = defaultdict(int)
-        self.current_time = 0
-        self.access_time = {}
+        self.frequency = {}
+        self.usage_order = {}
 
     def put(self, key, item):
-        """ Add an item to the cache """
+        """ Add an item in the cache """
         if key is None or item is None:
             return
 
-        self.frequency[key] += 1
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.frequency[key] += 1
+            self.usage_order[key] = self.usage_order.pop(key)
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lfu_keys = [
+                        k for k, v in self.frequency.items() if v == min(
+                            self.frequency.values()
+                            )
+                        ]
+                if len(lfu_keys) > 1:
+                    lru_key = min(lfu_keys, key=lambda k: self.usage_order[k])
+                else:
+                    lru_key = lfu_keys[0]
 
-        self.cache_data[key] = item
+                del self.cache_data[lru_key]
+                del self.frequency[lru_key]
+                del self.usage_order[lru_key]
+                print(f"DISCARD: {lru_key}")
 
-        self.access_time[key] = self.current_time
-        self.current_time += 1
-
-        if len(self.cache_data) > self.MAX_ITEMS:
-            min_frequency = min(self.frequency.values())
-            least_frequent_keys = [
-                    k for k, v in self.frequency.items() if v == min_frequency
-                    ]
-
-            lru_key = min(
-                    least_frequent_keys, key=lambda k: self.access_time[k])
-
-            del self.cache_data[lru_key]
-            del self.frequency[lru_key]
-            del self.access_time[lru_key]
-            print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.frequency[key] = 1
+            self.usage_order[key] = key
 
     def get(self, key):
-        """ Retrieve item from cache """
+        """ Get an item by key """
         if key is None or key not in self.cache_data:
             return None
 
         self.frequency[key] += 1
-        self.access_time[key] = self.current_time
-        self.current_time += 1
-
+        self.usage_order[key] = self.usage_order.pop(key)
         return self.cache_data[key]
 
 
