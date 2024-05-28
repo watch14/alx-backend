b4a803c31c2df7dcc8ea1f1d319fc785001c279a
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-28 20:19:59 +0100

    fast_push

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ language eng/fr """
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 class Config:
