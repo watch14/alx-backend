b7985f3d4a85a2f4878746f1c967e96b7811fb70
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-28 19:52:09 +0100

    fast_push

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" babel.localeselector """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config():
+    """ app config """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ get langue """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
