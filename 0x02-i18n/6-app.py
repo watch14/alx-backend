99b4354225303e99c6b6f0c129fa8802d85453fb
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-28 21:26:41 +0100

    fast_push

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+""" babel flastk """
+from flask_babel import Babel
+from flask import Flask, render_template, request, g
+from typing import Union, Dict
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """ app config """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+def get_user() -> Union[Dict, None]:
+    """ get user """
+    id = request.args.get('login_as', None)
+    if id and int(id) in users.keys():
+        return users.get(int(id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """ bef user """
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ get loco """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """ get index """
+    return render_template('6-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
